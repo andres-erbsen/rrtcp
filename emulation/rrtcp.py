d05d7d2f54857289c069086ecddf820dde74aa7e
Andres Erbsen <andreser@mit.edu>
2015-12-01 17:04:23 -0500

    switchless mininet configuration

--- emulation/rrtcp.py
+++ emulation/rrtcp.py
@@ -4,46 +4,41 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
-from mininet.util import dumpNodeConnections
+from mininet.util import dumpNodeConnections, waitListening
 from mininet.log import setLogLevel
 import time
 
-class SingleSwitchTopo(Topo):
-   "Single switch connected to n hosts."
-   def build(self, delay, loss):
-       switch = self.addSwitch('s1')
-       for h in range(2):
-           host = self.addHost('h%s' % (h + 1))
-           # 10 Mbps, 5ms delay, 10% loss, 1000 packet queue
-           self.addLink(host, switch,
-              bw=10, delay=str(delay/2) + 'ms', loss=loss, max_queue_size=1000, use_htb=True)
-
+timeToRun = 60
 def runTests():
     tcpTest = 'tcp-clock-station'
     udpTest = 'udp-clock-station'
     rrtcpTest = 'rrtcp-clock-station'
 
-    for delay in [0, 40, 80, 160]:
-        for loss in [0, 5, 10]:
+    for delay in [120]:# [0, 40, 80, 160]:
+        for loss in [3]:# [0, 5, 10]:
             runTest(tcpTest, delay, loss, 'tcp')
             runTest(udpTest, delay, loss, 'udp')
             runTest(rrtcpTest, delay, loss, 'rrtcp')
 
 
 def runTest(test, delay, loss, name):
-    timeToRun = 5 # seconds
-    topo = SingleSwitchTopo(delay, loss)
-    net = Mininet( topo=topo, link=TCLink )
+    net = Mininet()
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    net.addLink(h1, h2, cls=TCLink, bw=10, delay=str(delay) + 'ms', loss=loss, max_queue_size=1000, use_htb=True)
+
     net.start()
-
-    h1, h2 = net.get('h1', 'h2')
-    outputName = name + '_' + str(delay) + '_' + str(loss)+'.out'
-
-    h1.cmd( '../' + test + '/' + test + ' -l -d ' + str(timeToRun) + 's -address ' + h1.IP() + ':8080 2>'+outputName+'.listener.err &' )
-    h2.cmd( '../' + test + '/' + test + ' -d ' + str(timeToRun) + 's -address ' + h1.IP() + ':8080 > ' + outputName + ' 2>'+outputName+'.dialer.err &' )
-    time.sleep(timeToRun + .1)
-
+    takeMeasurements(h1, h2, test, delay, loss, name)
     net.stop()
+
+def takeMeasurements(h1, h2, test, delay, loss, name):
+    outputName = name + '_' + str(delay) + '_' + str(loss)+'.out'
+    print '*** starting listener...'
+    h1.cmd('../' + test + '/' + test + ' -l -d ' + str(timeToRun) + 's -address ' + h1.IP() + ':8080 2>'+outputName+'.listener.err &' )
+    time.sleep(.1)
+    print '*** running dialer...'
+    h2.cmd('../' + test + '/' + test + ' -d ' + str(timeToRun) + 's -address ' + h1.IP() + ':8080 > ' + outputName + ' 2>'+outputName+'.dialer.err' )
+    h1.cmd('killall '+name)
 
 if __name__ == '__main__':
    setLogLevel('info')
