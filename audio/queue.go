26cda2a6f48b3ab105806140d431e9913d3d7772
Olga Shestopalova <olgash@mit.edu>
2015-12-06 17:31:25 -0500

    fix to use only locks and add tests

--- audio/queue.go
+++ audio/queue.go
@@ -4,35 +4,37 @@ import (
 	"sync"
 )
 
+const messageDoesntExist = "NONE"
+
 type Queue struct {
 	sync.Mutex
-  messages    map[int]string
-	subscriber  chan<- interface{}
+  messages    map[int]interface{}
   lastPulled  int
 }
 
-func NewQueue(subscriber chan<- interface{}) *Queue {
-	return &Queue{subscriber: subscriber, lastPulled: -1, messages: make(map[int]string)}
+func NewQueue() *Queue {
+	return &Queue{lastPulled: -1, messages: make(map[int]interface{})}
 }
 
 // someone is asking for a message with an index, return it if
 // we have it and update lastPulled
-func (q *Queue) Pull(index int) {
+// Pull should be called in increasing index
+func (q *Queue) Pull(index int) interface{} {
 	q.Lock()
 	defer q.Unlock()
-  message, exists := q.messages[index]
-  if !exists{
-    //todo
-  } else {
-    q.subscriber <- message
-    delete(q.messages, index)
-  }
   q.lastPulled = index
+  message, exists := q.messages[index]
+  if !exists {
+    return messageDoesntExist
+  } else {
+    delete(q.messages, index)
+    return message
+  }
 }
 
 // put a message with an index into the queue
-// if the index is >= the last thing pulled, ignore it
-func (q *Queue) Put(index int, message string) {
+// if the index is < the last thing pulled, ignore it
+func (q *Queue) Push(index int, message interface{}) {
   q.Lock()
 	defer q.Unlock()
   // make sure it isn't a stale message
--- audio/queue_test.go
+++ audio/queue_test.go
@@ -2,11 +2,41 @@ package audio
 
 import (
 	"testing"
+  "fmt"
 )
 
 func Test(t *testing.T) {
-  ch := make(chan interface{}, 1)
-	queue := NewQueue(ch)
-  queue.Put(0, "abc")
-  queue.Pull(0)
+	queue := NewQueue()
+  fmt.Println("Push nothing")
+  if queue.Pull(1) != "NONE" {
+    t.Fail()
+  }
+
+  fmt.Println("Push in order")
+  queue.Push(2, "a")
+  queue.Push(3, "b")
+  queue.Push(4, "c")
+  if queue.Pull(2) != "a" {
+    t.Fail()
+  }
+  if queue.Pull(3) != "b" {
+    t.Fail()
+  }
+  if queue.Pull(4) != "c" {
+    t.Fail()
+  }
+
+  fmt.Println("Push out of order")
+  queue.Push(7, "f")
+  queue.Push(6, "e")
+  queue.Push(5, "d")
+  if queue.Pull(5) != "d" {
+    t.Fail()
+  }
+  if queue.Pull(6) != "e" {
+    t.Fail()
+  }
+  if queue.Pull(7) != "f" {
+    t.Fail()
+  }
 }
