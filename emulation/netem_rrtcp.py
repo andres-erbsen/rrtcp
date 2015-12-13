b13f44a596db8902c78cad847322a2495c05c2e4
Asya Bergal <asyabergal@gmail.com>
2015-12-13 04:56:02 -0500

    Attempting to log delays

--- emulation/netem_rrtcp.py
+++ emulation/netem_rrtcp.py
@@ -8,7 +8,7 @@ def runTests():
     for delay in delayIntervals:
         for loss in lossIntervals:
             runTest(tcpTestLocation, delay, loss, 'tcp')
-            runTest(udpTestLocation, delay, loss, 'udp')
+            #runTest(udpTestLocation, delay, loss, 'udp')
             runTest(rrtcpTestLocation, delay, loss, 'rrtcp')
 
 def runTest(testLocation, delay, loss, name):
@@ -17,6 +17,10 @@ def runTest(testLocation, delay, loss, name):
     outputName = name + '_' + str(delay) + '_' + str(loss)
 
     subprocess.call("tc qdisc change dev lo root netem delay " + str(delay) + "ms loss " + str(loss) + "%", shell=True)
+    time.sleep(.1)
+    print "Opening tcpdump"
+    subprocess.Popen("sudo tcpdump -i lo -tt port 8080 > " + outputName + ".tcpdump.out", shell=True)
+    print "Starting application"
     time.sleep(.1)
     subprocess.call( testLocation + ' -l -d ' + str(timeToRun) + 's -address localhost:8080 > ' + outputName + '.listener.out' + ' 2>'+outputName+'.listener.err &', shell=True)
     subprocess.call( testLocation + ' -d ' + str(timeToRun) + 's -address localhost:8080 > ' + outputName + '.dialer.out' + ' 2>'+outputName+'.dialer.err &', shell=True)
