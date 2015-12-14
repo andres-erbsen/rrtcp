b2d8faea55d0e1a6a5f6a3fd03284631dbaef940
Asya Bergal <asyabergal@gmail.com>
2015-12-13 20:17:20 -0500

    New data for tcpdump flight times

--- emulation/netem_rrtcp.py
+++ emulation/netem_rrtcp.py
@@ -12,18 +12,20 @@ def runTests():
             runTest(rrtcpTestLocation, delay, loss, 'rrtcp')
 
 def runTest(testLocation, delay, loss, name):
-    timeToRun = 30 # seconds
+    timeToRun = 10 # seconds
 
     outputName = name + '_' + str(delay) + '_' + str(loss)
 
-    subprocess.call("tc qdisc change dev lo root netem delay " + str(delay) + "ms loss " + str(loss) + "%", shell=True)
+    subprocess.call("tc qdisc change dev veth-1 root netem delay " + str(delay) + "ms loss " + str(loss) + "%", shell=True)
     time.sleep(.1)
     print "Opening tcpdump"
-    subprocess.Popen("sudo tcpdump -i lo -tt port 8080 > " + outputName + ".tcpdump.out", shell=True)
+    subprocess.Popen("sudo tcpdump -i veth-1 -tt -l -n | tee " + outputName + ".dialer.tcpdump.out", shell=True)
+    time.sleep(.1)
+    subprocess.Popen("sudo ip netns exec other tcpdump -i veth-2 -tt -l -n | tee " + outputName + ".listener.tcpdump.out", shell=True)
     print "Starting application"
     time.sleep(.1)
-    subprocess.call( testLocation + ' -l -d ' + str(timeToRun) + 's -address localhost:8080 > ' + outputName + '.listener.out' + ' 2>'+outputName+'.listener.err &', shell=True)
-    subprocess.call( testLocation + ' -d ' + str(timeToRun) + 's -address localhost:8080 > ' + outputName + '.dialer.out' + ' 2>'+outputName+'.dialer.err &', shell=True)
+    subprocess.call( testLocation + ' -l -d ' + str(timeToRun) + 's -address 10.1.1.1:2000 > ' + outputName + '.listener.out' + ' 2>'+outputName+'.listener.err &', shell=True)
+    subprocess.call( testLocation + ' -d ' + str(timeToRun) + 's -address 10.1.1.1 > ' + outputName + '.dialer.out' + ' 2>'+outputName+'.dialer.err &', shell=True)
     time.sleep(timeToRun + .1)
 
     print outputName
